ef4c951f3235fa8f15bda8a4e8bdd8fe5a8f5d78
rivalcoba <ivan.rivalcoba@gmail.com>
2021-10-09 15:39:25 -0500

    Adding Layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <nuxt-link :to="localePath('index')">{{ $t('home') }}</nuxt-link>
+      <nuxt-link :to="localePath('about')">{{ $t('about') }}</nuxt-link>
+      <nuxt-link :to="localePath('contact')">{{ $t('contact') }}</nuxt-link>
+    </div>
+    <div>
+      <language-input />
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <!-- <Tutorial/> -->
   <div class="container">
-    <div>
-      <nuxt-link :to="localePath('about')">{{ $t('about') }}</nuxt-link>
-      <nuxt-link :to="localePath('contact')">{{ $t('contact') }}</nuxt-link>
-    </div>
-    <div>
-      <language-input />
-    </div>
     <h1 class="title">{{ $t('message') }}</h1>
   </div>
 </template>
